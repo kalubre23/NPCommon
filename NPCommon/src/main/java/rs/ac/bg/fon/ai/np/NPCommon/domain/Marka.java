c8769e55c825e7487094d716da68d17191f947d5
kalubre23 <luka.zola@hotmail.com>
2024-04-06 17:51:58 +0200

    Testirana domenska klasa Marka

--- NPCommon/src/main/java/rs/ac/bg/fon/ai/np/NPCommon/domain/Marka.java
+++ NPCommon/src/main/java/rs/ac/bg/fon/ai/np/NPCommon/domain/Marka.java
@@ -14,25 +14,55 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * Predstavlja marku automobila.
+ * 
+ * Ima jedinstven identifikator i naziv.
  *
- * @author Asus
+ * @author Luka Obrenic
+ * @since 1.0.0
  */
 public class Marka extends DomenskiObjekat implements Serializable{
+	/**
+	 * Jedinstven identifikator marke automobila kao int.
+	 */
     private int markaID;
+    /**
+     * Naziv marke automobila kao string.
+     */
     private String naziv;
 
+    /**
+     * Prazan konstruktor za kreiranje jedne instance marke automobila sa podrazumevanim vrednostima za njegove atribute.
+     */
     public Marka() {
     }
 
+    /**
+     * Parametrizovani konstruktor koji sluzi za kreiranje instance marke automobila sa specificnim vrednostima za njegove atribute.
+     * 
+     * @param markaID - Jedinstven identifikator koji se dodeljuje instanci marke automobila pri kreiranju.
+     * @param naziv - Naziv marke koji se dodeljuje instanci marke automobila pri kreiranju.
+     */
     public Marka(int markaID, String naziv) {
         this.markaID = markaID;
         this.naziv = naziv;
     }
 
+    /**
+     * Vraca naziv marke automobila.
+     * @return naziv marke automobila kao string.
+     */
     public String getNaziv() {
         return naziv;
     }
 
+    /**
+     * Postavlja novu vrednost za naziv marke automobila.
+     * 
+     * @param naziv kao nova vrednost atributa naziv.
+     * @throws NullPointerException ako je uneta vrednost null.
+     * @throws IllegalArgumentException ako je uneta vrednost prazan string.
+     */
     public void setNaziv(String naziv) {
     	if(naziv==null) {
     		throw new NullPointerException("Naziv marke ne moze biti null");
@@ -43,21 +73,46 @@ public class Marka extends DomenskiObjekat implements Serializable{
         this.naziv = naziv;
     }
 
+    /**
+     * Vraca jedinstven identifikator marke automobila.
+     * @return markaID kao jednistveni identifikator marke automobila kao int.
+     */
     public int getMarkaID() {
         return markaID;
     }
 
+    /**
+     * Postavlja novu vrednost za jedinstveni identifikator marke automobila.
+     * 
+     * @param markaID kao nova vrednost atributa markaID.
+     * 
+     * @throws IllegalArgumentException ako je uneta vrednost manja od nule.
+     */
     public void setMarkaID(int markaID) {
     	if(markaID<0)
     		throw new IllegalArgumentException("MarkaID ne sme biti null");
         this.markaID = markaID;
     }
 
+    /**
+     * Vraca string reprezentaciju marke automobila na osnovu atributa naziv.
+     * 
+     * @return naziv kao string reprezentacija marke automobila.
+     */
     @Override
     public String toString() {
         return this.naziv;
     }
 
+    /**
+	 * Poredi dve marke automobila prema jedinstvenom identifikatoru.
+	 * 
+	 * @return
+	 * <ul>
+	 * 		<li> true ako su jedinstveni identifikatori isti </li>
+	 * 		<li> false ako je unet null, ako objekat nije klase Marka ili ako su razliciti identifikatori </li>
+	 * <ul>
+	 */
     @Override
     public boolean equals(Object obj) {
         if(!(obj instanceof Marka))
@@ -68,7 +123,7 @@ public class Marka extends DomenskiObjekat implements Serializable{
         
     }
     
-    
+ 
 
     @Override
     public String vratiNazivTabele() {
