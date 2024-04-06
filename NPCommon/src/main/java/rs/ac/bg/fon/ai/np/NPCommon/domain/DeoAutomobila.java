5cecd1cecd1b33721b696cd7114a5d988bb85ae8
kalubre23 <luka.zola@hotmail.com>
2024-04-06 17:43:40 +0200

    Dokumentovana domenska klasa DeoAutomobila

--- NPCommon/src/main/java/rs/ac/bg/fon/ai/np/NPCommon/domain/DeoAutomobila.java
+++ NPCommon/src/main/java/rs/ac/bg/fon/ai/np/NPCommon/domain/DeoAutomobila.java
@@ -14,25 +14,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Predstavlja deo automobila.
+ * 
+ * Ima jedinstven identifikator i naziv.
  *
- * @author Asus
+ * @author Luka Obrenic
+ * @since 1.0.0
  */
 public class DeoAutomobila extends DomenskiObjekat implements Serializable{
+	/**
+	 * Jedinstven identifikator dela automobila kao int.
+	 */
     private int deoID;
+    /**
+     * Naziv dela automobila kao string.
+     */
     private String naziv;
 
+    /**
+     * Prazan konstruktor za kreiranje jedne instance dela automobila sa podrazumevanim vrednostima za njegove atribute.
+     */
     public DeoAutomobila() {
     }
 
+    /**
+     * Parametrizovani konstruktor koji sluzi za kreiranje instance dela automobila sa specificnim vrednostima za njegove atribute.
+     * 
+     * @param deoID - Jedinstven identifikator koji se dodeljuje instanci dela automobila pri kreiranju.
+     * @param naziv - Naziv dela koji se dodeljuje instanci dela automobila pri kreiranju.
+     */
     public DeoAutomobila(int deoID, String naziv) {
         this.deoID = deoID;
         this.naziv = naziv;
     }
 
+    /**
+     * Vraca naziv dela automobila.
+     * @return naziv dela automobila kao string.
+     */
     public String getNaziv() {
         return naziv;
     }
 
+    /**
+     * Postavlja novu vrednost za naziv dela automobila.
+     * 
+     * @param naziv kao nova vrednost atributa naziv.
+     * @throws NullPointerException ako je uneta vrednost null.
+     * @throws IllegalArgumentException ako je uneta vrednost prazan string.
+     */
     public void setNaziv(String naziv) {
     	if(naziv==null) {
     		throw new NullPointerException("Naziv dela ne moze biti null");
@@ -43,21 +73,46 @@ public class DeoAutomobila extends DomenskiObjekat implements Serializable{
         this.naziv = naziv;
     }
 
+    /**
+     * Vraca jedinstven identifikator dela automobila.
+     * @return deoID kao jednistveni identifikator dela automobila kao int.
+     */
     public int getDeoID() {
         return deoID;
     }
 
+    /**
+     * Postavlja novu vrednost za jedinstveni identifikator dela automobila.
+     * 
+     * @param deoID kao nova vrednost atributa deoID.
+     * 
+     * @throws IllegalArgumentException ako je uneta vrednost manja od nule.
+     */
     public void setDeoID(int deoID) {
     	if(deoID < 0)
     		throw new IllegalArgumentException("DeoID ne sme biti < 0");
         this.deoID = deoID;
     }
 
+    /**
+     * Vraca string reprezentaciju dela automobila na osnovu atributa naziv.
+     * 
+     * @return naziv kao string reprezentacija dela automobila.
+     */
     @Override
     public String toString() {
         return this.naziv;
     }
 
+    /**
+	 * Poredi dva dela automobila prema jedinstvenom identifikatoru.
+	 * 
+	 * @return
+	 * <ul>
+	 * 		<li> true ako su jedinstveni identifikatori isti </li>
+	 * 		<li> false ako je unet null, ako objekat nije klase DeoAutomobila ili ako su razliciti identifikatori </li>
+	 * <ul>
+	 */
     @Override
     public boolean equals(Object obj) {
         if(!(obj instanceof DeoAutomobila))
