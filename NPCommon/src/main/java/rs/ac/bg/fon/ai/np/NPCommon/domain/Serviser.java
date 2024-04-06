124225152a7add9683ffcb4e96b251788d023ae2
kalubre23 <luka.zola@hotmail.com>
2024-04-06 19:36:55 +0200

    Dokumentovana domenska klasa Serviser

--- NPCommon/src/main/java/rs/ac/bg/fon/ai/np/NPCommon/domain/Serviser.java
+++ NPCommon/src/main/java/rs/ac/bg/fon/ai/np/NPCommon/domain/Serviser.java
@@ -14,20 +14,51 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * Predstavlja servisera koji radi u auto servisu.
+ * 
+ * Ima username i password pomocu kojih se loguje u sistem, ime, prezime kao i jedinstveni identifikator.
  *
- * @author Cartman
+ * @author Luka Obrenic
+ * @since 1.0.0
  */
 public class Serviser extends DomenskiObjekat implements Serializable {
 
+	/**
+	 * Jedinstveni identifikator servisera kao int.
+	 */
     private int serviserID;
+    /**
+     * Ime servisera kao string.
+     */
     private String ime;
+    /**
+     * Prezime servisera kao string.
+     */
     private String prezime;
+    /**
+     * Username za login kao string.
+     */
     private String username;
+    /**
+     * Password za login kao string.
+     */
     private String password;
 
+    /**
+     * Prazan konstruktor za kreiranje jedne instance servisera sa podrazumevanim vrednostima za njegove atribute.
+     */
     public Serviser() {
     }
 
+    /**
+     * Parametrizovani konstruktor koji sluzi za kreiranje instance servisera sa specificnim vrednostima za sve njegove atribute.
+     * 
+     * @param serviserID - Jedinstveni identifikator koji se dodeljuje instanci servisera pri kreiranju.
+     * @param ime - Ime koje se dodeljuje instanci servisera pri kreiranju.
+     * @param prezime - Prezime koje se dodeljuje instanci servisera pri kreiranju.
+     * @param username - Username koji se dodeljuje instanci servisera pri kreiranju.
+     * @param password - Password koje se dodeljuje instanci servisera pri kreiranju.
+     */
     public Serviser(int serviserID, String ime, String prezime,
          String username, String password) {
         this.serviserID = serviserID;
@@ -37,11 +68,24 @@ public class Serviser extends DomenskiObjekat implements Serializable {
         this.password = password;
     }
 
+    /**
+     * Parametrizovani konstruktor koji sluzi za kreiranje instance servisera sa specificnim vrednostima za atribute username i password.
+     * 
+     * @param username - Username koji se dodeljuje instanci servisera pri kreiranju.
+     * @param password - Password koje se dodeljuje instanci servisera pri kreiranju.
+     */
     public Serviser(String username, String password) {
         this.username = username;
         this.password = password;
     }
 
+    /**
+     * Vraca string reprezentaciju servisera.
+     * 
+     * Sastoji se od jedinstvenog identifikatora, imena, prezimena, username-a i sifre.
+     * 
+     * @return serviser kao string reprezentacija servisera u odgovarajucem formatu.
+     */
     @Override
     public String toString() {
         return "Serviser{" + "serviserID: " + serviserID + ", ime: " + ime + ", prezime: " + prezime + ", username: " + username + ", password: " + password + '}';
@@ -57,6 +101,17 @@ public class Serviser extends DomenskiObjekat implements Serializable {
 //        hash = 23 * hash + Objects.hashCode(this.password);
 //        return hash;
 //    }
+    
+    /**
+	 * Poredi dva servisera prema username-u i password-u.
+	 * 
+	 * 
+	 * @return
+	 * <ul>
+	 * 		<li> true ako je unet isti objekat ili ako su username i password isti </li>
+	 * 		<li> false ako je unet null, ako objekat nije klase Serviser ili ako su razliciti username i/ili password</li>
+	 * <ul>
+	 */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -75,10 +130,23 @@ public class Serviser extends DomenskiObjekat implements Serializable {
         return Objects.equals(this.password, other.password);
     }
 
+    /**
+     * Vraca jedinstveni identifikator servisera.
+     * @return serviserID identifikator kao int.
+     */
     public int getServiserID() {
         return serviserID;
     }
 
+    /**
+     * Postavlja novu vrednost za jedinstveni identifikator servisera.
+     * 
+     * Jedinstveni identifikator ne sme biti manji od nule.
+     * 
+     * @param serviserID kao nova vrednost identifikatora.
+     * 
+     * @throws IllegalArgumentException ako je uneta vrednost manja od nule.
+     */
     public void setServiserID(int serviserID) {
     	if(serviserID<0) {
     		throw new IllegalArgumentException("serviserID ne sme biti < 0");
@@ -86,10 +154,23 @@ public class Serviser extends DomenskiObjekat implements Serializable {
         this.serviserID = serviserID;
     }
 
+    /**
+     * Vraca ime servisera.
+     * @return ime servisera kao string.
+     */
     public String getIme() {
         return ime;
     }
 
+    /**
+	 * Postavlja novu vrednost atributa ime.
+	 * 
+	 * Ime ne sme biti null niti prazan string.
+	 * 
+	 * @param ime nova vrednost za ime servisera
+	 * @throws NullPointerException ako se unese null vrednost
+	 * @throws IllegalArgumentException ako se unese prazan string
+	 */
     public void setIme(String ime) {
     	if(ime == null) 
     		throw new NullPointerException("Ime servisera ne sme biti null");
@@ -98,10 +179,23 @@ public class Serviser extends DomenskiObjekat implements Serializable {
         this.ime = ime;
     }
 
+    /**
+     * Vraca prezime servisera.
+     * @return prezime servisera kao string.
+     */
     public String getPrezime() {
         return prezime;
     }
 
+    /**
+	 * Postavlja novu vrednost atributa prezime.
+	 * 
+	 * Prezime ne sme biti null niti prazan string.
+	 * 
+	 * @param prezime nova vrednost za prezime servisera
+	 * @throws NullPointerException ako se unese null vrednost
+	 * @throws IllegalArgumentException ako se unese prazan string
+	 */
     public void setPrezime(String prezime) {
     	if(prezime == null) 
     		throw new NullPointerException("Prezime servisera ne sme biti null");
@@ -110,10 +204,23 @@ public class Serviser extends DomenskiObjekat implements Serializable {
         this.prezime = prezime;
     }
 
+    /**
+     * Vraca username za logovanje servisera.
+     * @return username servisera kao string.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+	 * Postavlja novu vrednost atributa username.
+	 * 
+	 * Username ne sme biti null niti prazan string.
+	 * 
+	 * @param username nova vrednost za username servisera
+	 * @throws NullPointerException ako se unese null vrednost
+	 * @throws IllegalArgumentException ako se unese prazan string
+	 */
     public void setUsername(String username) {
     	if(username== null) 
     		throw new NullPointerException("Username servisera ne sme biti null!");
@@ -122,10 +229,23 @@ public class Serviser extends DomenskiObjekat implements Serializable {
         this.username = username;
     }
 
+    /**
+     * Vraca password za logovanje servisera.
+     * @return password servisera kao string.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+	 * Postavlja novu vrednost atributa password.
+	 * 
+	 * Password ne sme biti null niti prazan string.
+	 * 
+	 * @param password nova vrednost za password servisera
+	 * @throws NullPointerException ako se unese null vrednost
+	 * @throws IllegalArgumentException ako se unese prazan string
+	 */
     public void setPassword(String password) {
     	if(password== null) 
     		throw new NullPointerException("Password servisera ne sme biti null!");
