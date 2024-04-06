929cc775bc21d8bd1ed75d37c1992b8e9a0136e8
kalubre23 <luka.zola@hotmail.com>
2024-04-06 18:24:27 +0200

    Dokumentovana domenska klasa NalogZaServisiranje

--- NPCommon/src/main/java/rs/ac/bg/fon/ai/np/NPCommon/domain/NalogZaServisiranje.java
+++ NPCommon/src/main/java/rs/ac/bg/fon/ai/np/NPCommon/domain/NalogZaServisiranje.java
@@ -15,20 +15,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Predstavlja nalog za servisiranje automobila.
+ * 
+ * Nalog za servisiranje se odnosi na jedan uoceni kvar. Jedan uoceni kvar moze podrazumevati
+ * vise pokvarenih delova. Npr: uoceni kvar: problemi sa motorom, pokvareni delovi: akumulator, svecice.
+ * Ima jedinstven identifikator, datum servisiranja, cenu, podatak o tome na 
+ * koji se kvar odnosi kao i koji serviser je zakljucio nalog.
  *
- * @author Asus
+ * @author Luka Obrenic
+ * @since 1.0.0
  */
 public class NalogZaServisiranje extends DomenskiObjekat implements Serializable {
 
+	/**
+	 * Jedinstven identifikator naloga za servisiranje kao int.
+	 */
     private int nalogID;
+    /**
+     * Datum servisiranja automobila tipa LocalDate.
+     */
     private LocalDate datum;
+    /**
+     * Cena servisa kao double.
+     */
     private double cena;
+    /**
+     * Kvar na koji se odnosi nalog tipa UoceniKvar.
+     */
     private UoceniKvar kvar;
+    /**
+     * Serviser koji je zakljucio nalog kao instanca klase Serviser.
+     */
     private Serviser serviser;
 
+    /**
+     * Prazan konstruktor za kreiranje jedne instance naloga sa podrazumevanim vrednostima za njegove atribute.
+     */
     public NalogZaServisiranje() {
     }
 
+    /**
+     * Parametrizovani konstruktor koji sluzi za kreiranje instance naloga sa specificnim vrednostima za njegove atribute.
+     * @param nalogID - Jedinstven identifikator koji se dodeljuje instanci naloga pri kreiranju.
+     * @param datum - Datum servisiranja koji se dodeljuje instanci naloga pri kreiranju.
+     * @param cena - Cena servisa koje se dodeljuje instanci naloga pri kreiranju.
+     * @param kvar - Kvar na koji se odnosi servis, koji se dodeljuje instanci naloga pri kreiranju.
+     * @param serviser - Serviser koji je zakljucio nalog, koji se dodeljuje instanci naloga pri kreiranju.
+     */
     public NalogZaServisiranje(int nalogID, LocalDate datum, 
          double cena, UoceniKvar kvar, Serviser serviser) {
         this.nalogID = nalogID;
@@ -37,11 +70,21 @@ public class NalogZaServisiranje extends DomenskiObjekat implements Serializable
         this.kvar = kvar;
         this.serviser = serviser;
     }
-
+    
+    /**
+     * Vraca servisera koji je zakljucio nalog.
+     * @return serviser koji je zakljucio nalog tipa Serviser.
+     */
     public Serviser getServiser() {
         return serviser;
     }
 
+    /**
+     * Postavlja novu vrednost za atribut serviser.
+     * 
+     * @param serviser kao nova vrednost atributa serviser.
+     * @throws NullPointerException ako je uneta vrednost null.
+     */
     public void setServiser(Serviser serviser) {
     	if(serviser==null) {
     		throw new NullPointerException("Serviser ne moze biti null");
@@ -49,30 +92,63 @@ public class NalogZaServisiranje extends DomenskiObjekat implements Serializable
         this.serviser = serviser;
     }
 
+    /**
+     * Vraca kvar automobila na koji se nalog odnosi.
+     * @return kvar koji se odnosi na nalog tipa UoceniKvar.
+     */
     public UoceniKvar getKvar() {
         return kvar;
     }
 
+    /**
+     * Postavlja novu vrednost za atribut kvar.
+     * 
+     * @param kvar kao nova vrednost atributa kvar.
+     * @throws NullPointerException ako je uneta vrednost null.
+     */
     public void setKvar(UoceniKvar kvar) {
     	if(kvar == null)
     		throw new NullPointerException("Kvar kod naloga ne sme biti null!");
         this.kvar = kvar;
     }
 
+    /**
+     * Vraca jedinstevni identifikator naloga za servisiranje.
+     * @return nalogID identifikator tipa int.
+     */
     public int getNalogID() {
         return nalogID;
     }
 
+    /**
+     * Postavlja novu vrednost za jedinstveni identifikator naloga za servisiranje.
+     * 
+     * @param nalogID kao nova vrednost jedinstvenog identifikatora nalogID.
+     * 
+     * @throws IllegalArgumentException ako je uneta vrednost manja od nule.
+     */
     public void setNalogID(int nalogID) {
     	if(nalogID<0)
     		throw new IllegalArgumentException("NalogID ne sme biti < 0!");
         this.nalogID = nalogID;
     }
 
+    /**
+     * Vraca datum servisiranja tj datum kada je zakljucen(kreiran) nalog.
+     * @return datum servisiranja tipa LocalDate.
+     */
     public LocalDate getDatum() {
         return datum;
     }
 
+    /**
+     * Postavlja novu vrednost za datum servisiranja.
+     * 
+     * @param datum kao nova vrednost datuma.
+     * 
+     * @throws NullPointerException ako je uneta vrednost null.
+     * @throws IllegalArgumentException ako se uneti datum odnosi na buducnost.
+     */
     public void setDatum(LocalDate datum) {
     	if(datum == null)
     		throw new NullPointerException("Datum naloga ne sme biti null!");
@@ -81,23 +157,48 @@ public class NalogZaServisiranje extends DomenskiObjekat implements Serializable
         this.datum = datum;
     }
 
+    /**
+     * Vraca cenu servisa.
+     * @return cena servisa tipa kao double.
+     */
     public double getCena() {
         return cena;
     }
 
+    /**
+     * Postavlja novu vrednost cene servisa.
+     * 
+     * @param cena kao nova vrednost cene.
+     * 
+     * @throws IllegalArgumentException ako je uneta vrednost manja od nule.
+     */
     public void setCena(double cena) {
     	if(cena<0)
     		throw new IllegalArgumentException("Cena ne sme biti < 0!");
         this.cena = cena;
     }
 
-    ////
+    
+    /**
+     * Vraca string reprezentaciju naloga na osnovu jedinstvenog identifikatora i cene.
+     * 
+     * @return nalog kao string reprezentacija naloga u odgovarajucem formatu.
+     */
     @Override
     public String toString() {
         return "Nalogid: " + nalogID + ", cena: " + cena;
     }
     ////
 
+    /**
+	 * Poredi dva naloga za servisiranje prema jedinstvenom identifikatoru.
+	 * 
+	 * @return
+	 * <ul>
+	 * 		<li> true ako su identifikatori naloga isti </li>
+	 * 		<li> false ako je unet null, ako objekat nije klase NalogZaServisiranje ili ako su razliciti identifikatori </li>
+	 * <ul>
+	 */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
